e8f35e069533d8b175701b1232eaf5a45fa392d2
Brett <bwalter6@gmail.com>
2019-07-03 14:29:32 -0400

    added If.sh

---
+++ Shell/passwd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter zID"
+read -r ID
+
+zID=$ID
+
+echo "Enter Password:"
+read -rs p1
+
+zpass=$p1
+
+echo "//'$ID':'$p1'@dfw1mspif017.nao.global.gmacfs.com/public/mac/Build "
+
+
+
+
